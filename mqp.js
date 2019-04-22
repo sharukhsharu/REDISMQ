f0f309b29bbcc48ff079f47c5c545df5d4c47969
Sharukh <sharukhsharu@outlook.com>
2019-04-22 16:19:55 +0530

    Initial Commit
    Redis MQ --->  Implementation  with Workers

---
+++ mqp.js
@@ -0,0 +1,9 @@
+RedisSMQ = require("rsmq");
+rsmq = new RedisSMQ( {host: "127.0.0.1", port: 6379, ns: "rsmq"} );
+
+
+rsmq.sendMessage({qname:"myqueue", message:"Hello World"}, function (err, resp) {
+	if (resp) {
+		console.log("Message sent. ID:", resp);
+	}
+});
